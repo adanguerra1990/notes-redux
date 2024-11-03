8afb7f340c046470041a1484cc49f374e0043978
adanguerra1990 <aj90guerra@gmail.com>
2024-11-03 14:07:52 -0400

    Reducer combinados

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
-import noteReducer from './reducer/notesReducer.js'
+import noteReducer, { createNote } from './reducer/notesReducer.js'
+import filterReducer, { filterChange } from './reducer/filterReducer.js'
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+})
 
+const store = createStore(reducer)
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(
+  createNote('combineReducers forms one reducer from many simple reducers')
+)
+
+// createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// )
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <div />
   </Provider>
 )
