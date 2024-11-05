c24ec224aab459581fc14e25638ccb1a95b0ab59
adanguerra1990 <aj90guerra@gmail.com>
2024-11-05 09:59:38 -0400

    creando el Store usando la función configureStore de Redux Toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
 import noteReducer, { createNote } from './reducer/notesReducer.js'
 import filterReducer, { filterChange } from './reducer/filterReducer.js'
+import { configureStore } from '@reduxjs/toolkit'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
 })
 
-const store = createStore(reducer)
-
-// store.subscribe(() => console.log(store.getState()))
-// store.dispatch(filterChange('IMPORTANT'))
-// store.dispatch(
-//   createNote('combineReducers forms one reducer from many simple reducers')
-// )
+console.log(store.getState())
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
