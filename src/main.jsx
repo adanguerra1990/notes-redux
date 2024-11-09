3fd5e5c5c1fe20f53f6642d11de61c8744fd2ea7
adanguerra1990 <aj90guerra@gmail.com>
2024-11-09 10:58:32 -0400

    Separando el store

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
-import noteReducer from './reducer/notesReducer.js'
-import filterReducer from './reducer/filterReducer.js'
-import { configureStore } from '@reduxjs/toolkit'
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-})
-
-console.log(store.getState())
+import store from './store.js'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
